daba619f5b608e64be8f6168918772fe8ee5dba8
Emily <42111747+misaki0915@users.noreply.github.com>
2018-11-01 20:35:30 +0900

    Control the game continuations

--- beam_game.js
+++ beam_game.js
@@ -2,20 +2,23 @@ var your_pt = 0;
 var enemy_pt = 0;
 var judge = "〜続行〜";
 action_save.checked = true ;
+var game_end = document.getElementById("select");
+game_end.disabled = false;
 function resetGame() {
   your_pt = 0;
   enemy_pt = 0;
   judge = "〜続行〜";
+  game_end.disabled = false;
   document.getElementById("your_action").innerHTML = "";
   document.getElementById("enemy_action").innerHTML = "";
   document.getElementById("judge").innerHTML = "Game Start";
   document.getElementById("your_gauge").innerHTML = "自分のゲージは "+your_pt+"pt";
   document.getElementById("enemy_gauge").innerHTML = "相手のゲージは "+enemy_pt+"pt";
-  }
-  function myGame() {
-    action_save = document.form1.action_save.checked;
-    action_barrier = document.form1.action_barrier.checked;
-    action_beam = document.form1.action_beam.checked;
+}
+function myGame() {
+  action_save = document.form1.action_save.checked;
+  action_barrier = document.form1.action_barrier.checked;
+  action_beam = document.form1.action_beam.checked;
   if(your_pt===0 && action_beam === true){
     window.alert("ゲージが0ptなのでビームは選択できません！");
   } else {
@@ -59,12 +62,16 @@ function resetGame() {
     enemy_pt = enemy_pt -4;
   } else if (your_action == "ビーム" && your_pt >=4) {
     judge = "!!HIGH BEAM!!　　YOU WIN!!";
+  game_end.disabled = true;
   } else if (enemy_action == "ビーム" && enemy_pt >=4) {
     judge = "!!HIGH BEAM!!　　YOU LOSE...";
+  game_end.disabled = true;
   } else if (your_action == "ビーム" && enemy_action == "溜める") {
     judge = "YOU WIN!";
+  game_end.disabled = true;
   } else if (your_action == "溜める" && enemy_action == "ビーム") {
-      judge = "YOU LOSE...";
+    judge = "YOU LOSE...";
+  game_end.disabled = true;
   }
 document.getElementById("your_action").innerHTML = "自分の選択は "+your_action;
 document.getElementById("enemy_action").innerHTML = "相手の選択は "+enemy_action;
